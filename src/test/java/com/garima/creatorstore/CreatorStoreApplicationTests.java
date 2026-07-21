186b69674e99eba26ac3bd433fc75e093bba7d66
Garima Gupta <guptagarima466@gmail.com>
2026-07-22 00:18:56 +0530

    chore:setup of CreatorStore

---
+++ src/test/java/com/garima/creatorstore/CreatorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garima.creatorstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
